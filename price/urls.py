340db94ac4ceba92b5e79d5d9f118730ca846685
kentypop <Levimcc7@hotmail.com>
2021-08-15 21:38:27 +0900

    done detai. next price update

--- price/urls.py
+++ price/urls.py
@@ -2,9 +2,11 @@ from django.urls import path
 from .views import (
 	price_list_view,
 	price_create,
+	price_detail,
 	)
 
 urlpatterns = [
     path('list/', price_list_view , name='price_list'),
     path('new/', price_create , name='price_new'),
+    path('<int:id>/', price_detail , name='price_detail'),
     ]
--- price/views.py
+++ price/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.admin.views.decorators import staff_member_required
 from .models import Price
 from .forms import PriceForm
@@ -27,10 +27,16 @@ def price_create(request):
 			return redirect('price_list')
 	return render(request, 'price/price_create.html', {'form': form})	
 
-def price_detail(request):
-	return
+def price_detail(request, id):
+	#Another way of loading, using et_object_or_404 shows page not found if request failed
+	#obj= Price.objects.get(id=id)
+	obj= get_object_or_404(Price, id=id)
+	context= {'object': obj}
+	return render(request, 'price/price_detail.html', context)
 
-def price_update(request):
+def price_update(request, id):
+	obj= get_object_or_404(Price, id=id)
+	#NOWWWWWWWWWWW
 	return
 
 def price_delete(request):
